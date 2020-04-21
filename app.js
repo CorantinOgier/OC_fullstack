fcdcbc7fd88b25fa31a8c8347b19a31e89c5928d
Corantin Ogier <corantinogier@gmail.com>
2020-04-21 14:48:47 +0200

    connect API to BDD

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb+srv://CorantinO:MongoDB33000@cluster0-3gsiy.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
