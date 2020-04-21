89af03324149ce6c679ce283392067315dc1c6d4
Corantin Ogier <corantinogier@gmail.com>
2020-04-21 18:10:02 +0200

    create login function

--- controllers/user.js
+++ controllers/user.js
@@ -17,5 +17,22 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-
-};
+    User.findOne({ email: req.body.email }) // fonction asynchrone // trouver un utilisateur
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        }
+        bcrypt.compare(req.body.password, user.password) // comparer mdp et hash
+          .then(valid => {
+            if (!valid) {
+              return res.status(401).json({ error: 'Mot de passe incorrect !' }); // user trouvé mais comparaison renvoie false
+            }
+            res.status(200).json({
+              userId: user._id,
+              token: 'TOKEN' // on verra pour générer un token crypter plus tard
+            });
+          })
+          .catch(error => res.status(500).json({ error })); 
+      })
+      .catch(error => res.status(500).json({ error })); // problème de connexion, problème liée à MongoDB, etc
+  };
