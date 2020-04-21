ae1f47a388851ba62ae1fbcef750decccbafb159
Corantin Ogier <corantinogier@gmail.com>
2020-04-21 12:42:27 +0200

    add POST route request

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -7,7 +8,15 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body)
+    res.status(201).json({message: 'Objet créé !'
   });
+});
 
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
@@ -29,6 +38,6 @@ app.use('/api/stuff', (req, res, next) => {
       },
     ];
     res.status(200).json(stuff);
-  });
+});
 
 module.exports = app;
