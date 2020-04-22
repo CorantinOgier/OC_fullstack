ff0ad81a05f2e4b764cfc618ecbcc2470ed3e273
Corantin Ogier <corantinogier@gmail.com>
2020-04-22 11:35:05 +0200

    modify PUT route request

--- routes/stuff.js
+++ routes/stuff.js
@@ -7,7 +7,7 @@ const multer = require('../middleware/multer-config');
 
 // lorsqu'on veut protéger une route, on rajoute le middleware avant le controller
 router.post('/', auth, multer, stuffCtrl.createThing); // on veut protéger la création // toujours mettre multer après la const d'auth, sinon même une requête non identifiée serait saved
-router.put('/:id', auth, stuffCtrl.modifyThing); // la modification
+router.put('/:id', auth, multer, stuffCtrl.modifyThing); // la modification
 router.delete('/:id', auth, stuffCtrl.deleteThing); // la suppression
 router.get('/:id', auth, stuffCtrl.getOneThing); // aller chercher le stuff dans la base
 router.get('/', auth, stuffCtrl.getAllThings);
